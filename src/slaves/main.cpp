b2c1767121f46b488a22318b5fcddaa78e1b9767
Luke Hoggett <luke.hoggett@gmail.com>
2017-01-06 16:18:01 +1100

    split slaves into cpp and h files

--- src/slaves/main.cpp
+++ src/slaves/main.cpp
@@ -1,20 +1,4 @@
-// includes required for conversion to cpp files
-#include <Arduino.h>
-
-
-#include <Wire.h>                // wire library for I2C
-#include <Colorduino.h>          // colorduino library
-
-#define I2C_DEVICE_ADDRESS 0x05  // I2C address for this device
-#define START_OF_DATA 0x10       // data markers
-#define END_OF_DATA 0x20         // data markers
-
-// declarations required for conversion to cpp file, should move to .h file
-void setWhiteBalance();
-void drawDots();
-void listener();
-void receiveEvent(int numBytes);
-
+#include "main.h"
 
 unsigned char whiteBalanceValue[3] = {20, 63, 63};
 
